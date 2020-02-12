1eb537a0f26a622098a81e581e7d5ecf7fa6b86f
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-02-12 14:45:35 -0700

    added .get

---
+++ blogs/posts-router.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const Posts = require('../data/db.js')
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    Posts.find()
+    .then(posts => {
+        res.status(200).json(posts)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Couldn't retrieve posts"
+        })
+    })
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    Posts.findById(id)
+    .then(post => {
+        !post[0]
+        ? res.status(404.json({ errorMessage: "Id does not exist"}))
+        : res.status(200).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Couldn't retrieve posts"
+        })
+    })
+})
+
