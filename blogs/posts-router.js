7ee84edc1cb96170a4a0c895b9c015924828277c
Kristin Parrish <kristinparrish@Kristins-MacBook-Pro.local>
2020-02-12 15:07:00 -0700

    .posts added

--- blogs/posts-router.js
+++ blogs/posts-router.js
@@ -21,7 +21,7 @@ router.get('/:id', (req, res) => {
     Posts.findById(id)
     .then(post => {
         !post[0]
-        ? res.status(404).json({ errorMessage: "Id does not exist"}))
+        ? res.status(404).json({ errorMessage: "ID does not exist"})
         : res.status(200).json(post)
     })
     .catch(err => {
@@ -39,7 +39,7 @@ router.get('/:id/comments', (req, res) => {
     .then(comment => {
         comment[0]
         ? res.status(200).json(comment)
-        : res.status(404).json({ errorMessage: "Comment ID does not exist"})
+        : res.status(404).json({ errorMessage: "ID does not exist"})
     })
     .catch(err => {
         console.log(err)
@@ -49,3 +49,49 @@ router.get('/:id/comments', (req, res) => {
     })
 })
 
+
+router.post('/', (req, res) => {
+    const payload = req.body
+
+    !payload.title || !payload.contents
+    ? res.status(400).json({errorMessage: "Please include Title and Content for post"})
+    : Posts.insert(payload)
+    .then(post => {
+        res.status(201).json(post)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            errorMessage: "Error saving post"
+        })
+    })
+})
+
+
+router.post('/:id/comments', (req, res) => {
+    const { id } = req.params
+    const payload = {...req.body, post_id: id}
+
+    if(payload.text){
+    Posts.findById(id)
+    .then(post => {
+        !post[0]
+        ? res.status(404).json({ errorMessage: "ID does not exist"})
+        : Posts.insertComment(payload)
+            .then(comment => {
+            res.status(201).json(comment)
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                    errorMessage: "Error saving to the database"
+                })
+            })
+    .catch (err => {
+        res.status(500).json ({ errorMessage: "Couldn't be retrieved" })
+    })
+    })
+    }else{
+        res.status(400).json({errorMessage: "Please provide text for the comment"})
+    }
+})
